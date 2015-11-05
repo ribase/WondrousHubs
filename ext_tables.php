e5f295440b55c0af46521cab780111f99b8d1859
sebastian <baschdi@wondrous.ch>
2015-11-05 09:27:42 +0100

    renamed example

--- ext_tables.php
+++ ext_tables.php
@@ -3,7 +3,5 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Wondrous Hubs');
-
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('Vendor.WondrousHubs', 'Content');
 
