d8136f1c1d598844c32adbed766be745406b04f4
sebastian <baschdi@wondrous.ch>
2015-11-05 09:37:42 +0100

    added namespace

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,7 @@
 // ext_localconf.php
+
+\FluidTYPO3\Flux\Core::registerProviderExtensionKey('Vendor.WondrousHubs', 'Content');
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY, 'setup', '
     plugin.tx_myextensionkey.view {
         templateRootPaths.10 = EXT:wondrous_hubs/Resources/Private/Templates/
