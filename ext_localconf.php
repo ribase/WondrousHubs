d2f2cecfd95ede326664b91a4f45709b150c5a41
sebastian <baschdi@wondrous.ch>
2015-11-05 09:40:45 +0100

    rename test

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('Vendor.WondrousHubs', 'Content');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY, 'setup', '
-    plugin.tx_myextensionkey.view {
+    plugin.tx_wondroushubs.view {
         templateRootPaths.10 = EXT:wondrous_hubs/Resources/Private/Templates/
         partialRootPaths.10 = EXT:wondrous_hubs/Resources/Private/Partials/
         layoutRootPaths.10 = EXT:wondrous_hubs/Resources/Private/Layouts/
